b349a02a6c9258e91a2dc9613c70c5d70f3633c9
kez542 <kez542@kez542s-MacBook-Air.local>
2025-03-25 13:02:35 +0100

    first try main+info

--- info.py
+++ info.py
@@ -1,67 +1,101 @@
-import tkinter as tk
-from tkinter import ttk
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QLabel, QFrame, QScrollArea)
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 
-import sys
-
-if sys.platform == "win32":
-    from ctypes import windll
-
-    # To avoid blurry fonts
-    from ctypes import windll
-    windll.shcore.SetProcessDpiAwareness(1)
-else:
-    windll = None  # Or handle it differently for macOS
-
-
-class Info(tk.Frame):
+class Info(QWidget):
     def __init__(self, master, controller):
-        tk.Frame.__init__(self, master)
+        super().__init__(master)
         self.controller = controller
         self.master = master
-        self.infoWindow()
-
-    def infoWindow(self):
-        iw = tk.Toplevel()
-        iw.resizable(False, False)
-        iw.title('Information')
-        iw.iconbitmap('icons/icon.ico')
-        iw.lift() # Place the toplevel window at the top
-        # self.cm.windowGeometry(iw, 850, 250)
-
-        # LABELS
-        lab_titl = ttk.Label(iw, text='Signal Visualizer', font=('TkDefaultFont', 12))
-        lab_intr = ttk.Label(iw, text='A collaboration between University of the Basque Country (UPV/EHU)\nand Musikene, Higher School of Music of the Basque Country.\n')
+        self.initUI()
         
-        lab_lea1 = ttk.Label(iw, font=('TkDefaultFont', 9, "italic"), text='Project leader:')
-        lab_con1 = ttk.Label(iw, font=('TkDefaultFont', 9, "italic"), text='Project leader from Musikene:')
-        lab_dev1 = ttk.Label(iw, font=('TkDefaultFont', 9, "italic"), text='Developers:')
-        lab_ico1 = ttk.Label(iw, font=('TkDefaultFont', 9, "italic"), text='Program icon and logo made by:')
-
-        lab_lea2 = ttk.Label(iw, text='Inma Hernáez Rioja')
-        lab_con2 = ttk.Label(iw, text='José María Bretos')
-        lab_dev2 = ttk.Label(iw, text='Leire Varela Aranguren, Valentin Lurier, Eder del Blanco Sierra, Mikel Díez García')
-        lab_ico2 = ttk.Label(iw, text='Sergio Santamaría Martínez')
-
-        lab_ahol = ttk.Label(iw, text='\nHiTZ Basque Center for Language Technologies - Aholab Signal Processing Laboratory (UPV/EHU).\n')
-        lab_refe = ttk.Label(iw, text='References:')
-        lab_ref1 = ttk.Label(iw, text='Function for creating brown (or red) noise made by Hristo Zhivomirov:\n'+'Hristo Zhivomirov (2020). Pink, Red, Blue and Violet Noise Generation with Matlab.\n'+'https://www.mathworks.com/matlabcentral/fileexchange/42919-pink-red-blue-and-violet-noise-generation-with-matlab\n'+'MATLAB Central File Exchange. Retrieved August 4, 2020.')
-        lab_ref2 = ttk.Label(iw, text='Master thesis describing the version of the software Signal Visualizer in Matlab made by Eder del Blanco Sierra:\n'+'Eder del Blanco Sierra (2020). Programa de apoyo a la enseñanza musical.\n'+'University of the Basque Country (UPV/EHU). Department of Communications Engineering. Retrieved August 8, 2020.\n'+'The function has been modified by Valentin Lurier and Mikel Díez García.')
-
-        # positioning Labels
-        lab_titl.grid(column=0, row=0, sticky=tk.W, columnspan=40, padx=20, pady=10)
-        lab_intr.grid(column=0, row=1, sticky=tk.W, columnspan=40, padx=20)
-
-        lab_lea1.grid(column=0, row=2, sticky=tk.W, padx=20)
-        lab_con1.grid(column=0, row=3, sticky=tk.W, padx=20)
-        lab_dev1.grid(column=0, row=4, sticky=tk.W, padx=20)
-        lab_ico1.grid(column=0, row=5, sticky=tk.W, padx=20)
-
-        lab_lea2.grid(column=1, row=2, sticky=tk.W, padx=20)
-        lab_con2.grid(column=1, row=3, sticky=tk.W, padx=20)
-        lab_dev2.grid(column=1, row=4, sticky=tk.W, padx=20)
-        lab_ico2.grid(column=1, row=5, sticky=tk.W, padx=20)
-
-        lab_ahol.grid(column=0, row=7, sticky=tk.W, columnspan=40, padx=20)
-        lab_refe.grid(column=0, row=8, sticky=tk.W, columnspan=40, padx=20)
-        lab_ref1.grid(column=0, row=9, sticky=tk.W, columnspan=40, padx=20)
-        lab_ref2.grid(column=0, row=10, sticky=tk.W, columnspan=40, padx=20, pady=20)
+    def initUI(self):
+        # Create a scroll area to handle long content
+        scroll = QScrollArea()
+        scroll.setWidgetResizable(True)
+        scroll.setFrameShape(QFrame.NoFrame)
+        
+        # Main container widget
+        container = QWidget()
+        layout = QVBoxLayout(container)
+        layout.setContentsMargins(20, 20, 20, 20)
+        layout.setSpacing(10)
+        
+        # Title
+        title = QLabel("Signal Visualizer")
+        title_font = QFont()
+        title_font.setPointSize(12)
+        title_font.setBold(True)
+        title.setFont(title_font)
+        layout.addWidget(title)
+        
+        # Introduction
+        intro = QLabel("A collaboration between University of the Basque Country (UPV/EHU)\n"
+                      "and Musikene, Higher School of Music of the Basque Country.")
+        layout.addWidget(intro)
+        layout.addSpacing(10)
+        
+        # Team information
+        self.add_label_pair(layout, "Project leader:", "Inma Hernáez Rioja")
+        self.add_label_pair(layout, "Project leader from Musikene:", "José María Bretos")
+        self.add_label_pair(layout, "Developers:", "Leire Varela Aranguren, Valentin Lurier, Eder del Blanco Sierra, Mikel Díez García")
+        self.add_label_pair(layout, "Program icon and logo made by:", "Sergio Santamaría Martínez")
+        
+        # Add spacing
+        layout.addSpacing(20)
+        
+        # Aholab information
+        aholab = QLabel("HiTZ Basque Center for Language Technologies - Aholab Signal Processing Laboratory (UPV/EHU).")
+        layout.addWidget(aholab)
+        
+        # References title
+        ref_title = QLabel("References:")
+        ref_title.setFont(title_font)
+        layout.addWidget(ref_title)
+        
+        # References content
+        ref1 = QLabel(
+            "Function for creating brown (or red) noise made by Hristo Zhivomirov:\n"
+            "Hristo Zhivomirov (2020). Pink, Red, Blue and Violet Noise Generation with Matlab.\n"
+            "https://www.mathworks.com/matlabcentral/fileexchange/42919-pink-red-blue-and-violet-noise-generation-with-matlab\n"
+            "MATLAB Central File Exchange. Retrieved August 4, 2020."
+        )
+        ref1.setWordWrap(True)
+        layout.addWidget(ref1)
+        
+        ref2 = QLabel(
+            "Master thesis describing the version of the software Signal Visualizer in Matlab made by Eder del Blanco Sierra:\n"
+            "Eder del Blanco Sierra (2020). Programa de apoyo a la enseñanza musical.\n"
+            "University of the Basque Country (UPV/EHU). Department of Communications Engineering. Retrieved August 8, 2020.\n"
+            "The function has been modified by Valentin Lurier and Mikel Díez García."
+        )
+        ref2.setWordWrap(True)
+        layout.addWidget(ref2)
+        
+        # Add stretch to push content up
+        layout.addStretch()
+        
+        # Set the scroll area's widget
+        scroll.setWidget(container)
+        
+        # Main layout for this widget
+        main_layout = QVBoxLayout(self)
+        main_layout.addWidget(scroll)
+        self.setLayout(main_layout)
+    
+    def add_label_pair(self, layout, label_text, value_text):
+        """Helper method to add label-value pairs with consistent styling"""
+        container = QWidget()
+        h_layout = QVBoxLayout(container)
+        h_layout.setContentsMargins(0, 0, 0, 0)
+        
+        label = QLabel(label_text)
+        label_font = QFont()
+        label_font.setItalic(True)
+        label.setFont(label_font)
+        
+        value = QLabel(value_text)
+        
+        h_layout.addWidget(label)
+        h_layout.addWidget(value)
+        layout.addWidget(container)
